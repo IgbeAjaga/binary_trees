e2c3bf451b363ef03542c26cc9334267cd89d7f8
IgbeAjaga <ajagaigbe144@gmail.com>
2023-08-02 08:07:42 -0700

    betty update

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -6,16 +6,15 @@
  *
  * @node: Pointer to the node to find the sibling
  *
- * Return: Pointer to the sibling node, or NULL if node is NULL or the parent is NULL
+ * Return: Pointer to sibling node, or NULL if node is NULL or parent is NULL
  *         If node has no sibling, return NULL
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    if (node == NULL || node->parent == NULL)
-        return (NULL);
-
-    if (node->parent->left == node)
-        return (node->parent->right);
-    else
-        return (node->parent->left);
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	else
+		return (node->parent->left);
 }
