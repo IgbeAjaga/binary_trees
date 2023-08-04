5b4469861fe14d6665b25bd9418eb4810684cb32
IgbeAjaga <ajagaigbe144@gmail.com>
2023-08-03 19:27:30 -0700

    update

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -1,8 +1,50 @@
 /**
- * bst_remove - remove node from BST tree
- * @root: root of the tree
- * @value: node with this value to remove
- * Return: the tree changed
+ * remove_type - Removes a node
+ * depending on its children
+ * @node: Node to remove
+ * Return: 0 if it has no children, or other value if otherwise
+ */
+int remove_type(bst_t *node)
+{
+	if (!node->left && !node->right)
+	{
+		if (node->parent->right == node)
+			node->parent->right = NULL;
+		else
+			node->parent->left = NULL;
+		free(node);
+		return (0);
+	}
+	else if ((!node->left && node->right) || (!node->right && node->left))
+	{
+		if (!node->left)
+		{
+			if (node->parent->right == node)
+				node->parent->right = node->right;
+			else
+				node->parent->left = node->right;
+			node->right->parent = node->parent;
+		}
+		if (!node->right)
+		{
+			if (node->parent->right == node)
+				node->parent->right = node->left;
+			else
+				node->parent->left = node->left;
+			node->left->parent = node->parent;
+		}
+		free(node);
+		return (0);
+	}
+	else
+		return (get_successor(node->right));
+}
+
+/**
+ * bst_remove - Remove a node from a BST tree
+ * @root: Root of the tree
+ * @value: Value of the node to remove
+ * Return: The updated tree
  */
 bst_t *bst_remove(bst_t *root, int value)
 {
@@ -10,6 +52,7 @@ bst_t *bst_remove(bst_t *root, int value)
 
 	if (root == NULL)
 		return (NULL);
+
 	if (value < root->n)
 		root->left = bst_remove(root->left, value);
 	else if (value > root->n)
@@ -22,5 +65,6 @@ bst_t *bst_remove(bst_t *root, int value)
 	}
 	else
 		return (NULL);
+
 	return (root);
 }
