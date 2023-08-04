7532e0650a2a40f31533a0b8ef708093179c798e
IgbeAjaga <ajagaigbe144@gmail.com>
2023-08-03 18:47:14 -0700

    update

--- 122-array_to_avl.c
+++ 122-array_to_avl.c
@@ -10,21 +10,29 @@
  */
 avl_t *array_to_avl(int *array, size_t size)
 {
-    size_t i;
-    avl_t *root = NULL;
+	size_t i, j = 0;
+	avl_t *root;
 
-    if (array == NULL)
-        return (NULL);
-
-    for (i = 0; i < size; i++)
-    {
-        if (avl_insert(&root, array[i]) == NULL)
-        {
-            /* If insertion fails, free the tree and return NULL */
-            binary_tree_delete(root);
-            return (NULL);
-        }
-    }
-
-    return (root);
+	root = NULL;
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			if (array[j] == array[i])
+				break;
+		}
+		if (j == i)
+		{
+			if (avl_insert(&root, array[i]) == NULL)
+			{
+				return (NULL);
+			}
+		}
+	}
+	return (root);
 }
+
