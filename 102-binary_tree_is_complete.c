fba09fe9ff4bcc74a964c04a71c4babd6258bfbd
IgbeAjaga <ajagaigbe144@gmail.com>
2023-08-03 19:55:10 -0700

    update

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,103 +1,113 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "binary_trees.h"
 
 /**
- * binary_tree_enqueue - Enqueues a node in a binary tree queue
+ * create_node - Creates a new node for
+ * the linked list.
+ * @node: Pointer to the binary tree node.
  *
- * @rear: Pointer to the rear of the queue
- * @node: Pointer to the node to enqueue
+ * Return: Pointer to the new node.
  */
-void binary_tree_enqueue(binary_tree_t **rear, const binary_tree_t *node)
+link_t *create_node(binary_tree_t *node)
 {
-    binary_tree_t *new = (binary_tree_t *)node;
-
-    if (rear == NULL)
-        return;
-
-    if (*rear == NULL)
-    {
-        new->next = new;
-        *rear = new;
-    }
-    else
-    {
-        new->next = (*rear)->next;
-        (*rear)->next = new;
-        *rear = new;
-    }
+	link_t *new_node = malloc(sizeof(link_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->node = node;
+	new_node->next = NULL;
+	return (new_node);
 }
 
 /**
- * binary_tree_dequeue - Dequeues a node from a binary tree queue
- *
- * @rear: Pointer to the rear of the queue
- *
- * Return: Pointer to the dequeued node
+ * free_queue - Frees the nodes in the linked list.
+ * @head: Pointer to the head node of the linked list.
  */
-binary_tree_t *binary_tree_dequeue(binary_tree_t **rear)
+void free_queue(link_t *head)
 {
-    binary_tree_t *front;
-
-    if (rear == NULL || *rear == NULL)
-        return (NULL);
-
-    front = (*rear)->next;
-
-    if (*rear == front)
-        *rear = NULL;
-    else
-        (*rear)->next = front->next;
-
-    front->next = NULL;
-    return (front);
+	link_t *temp_node;
+	while (head)
+	{
+		temp_node = head->next;
+		free(head);
+		head = temp_node;
+	}
 }
 
 /**
- * binary_tree_is_complete - Checks if a binary tree is complete
+ * push - Pushes a node into the stack (linked list).
+ * @node: Pointer to the binary tree node to push.
+ * @head: Pointer to the head node of the stack.
+ * @tail: Pointer to the tail node of the stack.
+ */
+void push(binary_tree_t *node, link_t *head, link_t **tail)
+{
+	link_t *new_node = create_node(node);
+	if (new_node == NULL)
+	{
+		free_queue(head);
+		exit(1);
+	}
+	(*tail)->next = new_node;
+	*tail = new_node;
+}
+
+/**
+ * pop - Pops a node from the stack (linked list).
+ * @head: Pointer to the head node of the stack.
+ */
+void pop(link_t **head)
+{
+	link_t *temp_node = (*head)->next;
+	free(*head);
+	*head = temp_node;
+}
+
+/**
+ * binary_tree_is_complete - Checks if
+ * a binary tree is complete.
+ * @tree: Pointer to the binary tree node.
  *
- * @tree: Pointer to the root node of the tree to check
- *
- * Return: 1 if the tree is complete, 0 otherwise
+ * Return: 1 if the tree is complete, otherwise 0.
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-    binary_tree_t *rear = NULL;
-    int flag = 0;
+	link_t *head, *tail;
+	int flag = 0;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    binary_tree_enqueue(&rear, tree);
+	head = tail = create_node((binary_tree_t *)tree);
+	if (head == NULL)
+		exit(1);
 
-    while (rear != NULL)
-    {
-        binary_tree_t *current = binary_tree_dequeue(&rear);
+	while (head != NULL)
+	{
+		if (head->node->left != NULL)
+		{
+			if (flag == 1)
+			{
+				free_queue(head);
+				return (0);
+			}
+			push(head->node->left, head, &tail);
+		}
+		else
+			flag = 1;
 
-        if (current != NULL)
-        {
-            if (flag)
-            {
-                if (current->left != NULL || current->right != NULL)
-                    return (0);
-            }
-            else
-            {
-                if (current->left == NULL && current->right != NULL)
-                    return (0);
+		if (head->node->right != NULL)
+		{
+			if (flag == 1)
+			{
+				free_queue(head);
+				return (0);
+			}
+			push(head->node->right, head, &tail);
+		}
+		else
+			flag = 1;
 
-                if (current->left != NULL)
-                    binary_tree_enqueue(&rear, current->left);
-                else
-                    flag = 1;
+		pop(&head);
+	}
 
-                if (current->right != NULL)
-                    binary_tree_enqueue(&rear, current->right);
-                else
-                    flag = 1;
-            }
-        }
-    }
-
-    return (1);
+	return (1);
 }
